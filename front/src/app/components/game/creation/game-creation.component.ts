4c78fd76b39ca90ae05ed1f444c93eb41d4dbfbc
coreders <coreders@users.noreply.github.com>
2021-02-07 17:47:03 +0100

    copy button for invite url

--- front/src/app/components/game/creation/game-creation.component.ts
+++ front/src/app/components/game/creation/game-creation.component.ts
@@ -7,9 +7,21 @@ import {LoginService, UserCreationForm} from "../../../services/login.service";
     styleUrls: ['./game-creation.component.css']
 })
 export class GameCreationComponent {
-    inviteUrl: string = ''
+    inviteUrl: string = 'http://todo?game=todo'
 
     constructor(public loginService: LoginService) {
+    }
+
+    copyUrlToClipboard() {
+        console.log("copy "+this.inviteUrl)
+        var input = document.querySelector('#copy-input');
+        if(input) {
+            // @ts-ignore
+            input.select();
+            // @ts-ignore
+            input.setSelectionRange(0, input.value.length + 1);
+            document.execCommand('copy')
+        }
     }
 
     start() {
